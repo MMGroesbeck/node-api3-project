4d2c59992bb3f44882fe72808db7f18567cf5e9f
M Groesbeck <m.groesbeck@gmail.com>
2020-04-08 13:59:56 -0700

    GET requests for root and /api/users added and tested.

--- users/userRouter.js
+++ users/userRouter.js
@@ -13,15 +13,31 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  UserDb.get()
+  .then(users => {
+    res.status(200).json(users);
+  })
+  .catch(err => {
+    res.status(500).json({ message: "Error retrieving users." });
+  })
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-  // do your magic!
+  res.status(200).json(req.user);
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
-  // do your magic!
+  UserDb.getUserPosts(req.params.id)
+  .then(posts => {
+    if(posts){
+      res.status(200).json(posts);
+    } else {
+      res.status(404).json({ message: `No posts found for ${req.user.name}.`});
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: "Error retrieving posts." });
+  })
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
